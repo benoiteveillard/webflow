b4647b142fa142b550708e89eab85c23e5684cff
Benoît Eveillard <32536347+benoiteveillard@users.noreply.github.com>
2022-12-07 09:16:32 +0100

    "new console

--- new.js
+++ new.js
@@ -1,2 +1,4 @@
 document.getElementById("test");
 newText.innerHTML = "un beau paragraphe"
+
+https://cdn.jsdelivr.net/gh/benoiteveillard/webflow@2eaf016692e1b23c656db01a4b6ab98ff85d48b6/tests.js
--- tests.js
+++ tests.js
@@ -1,3 +1,1 @@
-const webflow = 2;
-const test = document.getElementById("webflow");
-console.log("webflow");
+console.log("hey Benoît");
